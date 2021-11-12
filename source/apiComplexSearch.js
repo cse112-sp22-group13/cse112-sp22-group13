49ff87b16e0939e9e48d8c2323cdd8fe0237a386
Xavier Sevcik <5400808+xsevcik@users.noreply.github.com>
2021-11-12 12:49:40 -0800

    Refactor apiComplexSearch to follow style

--- source/apiComplexSearch.js
+++ source/apiComplexSearch.js
@@ -1,64 +1,93 @@
 // put "import { complexSearch_c } from './apiComplexSearch.js';" in main.js without quotes
+import axios from 'axios'
 /**
  * @param {string} input
  *
  */
-export class complexSearch_c {
+export class cComplexSearch {
    data = {};
-   static bread_t = {
-      WAFFLE: 'waffle ',
-      BUN: 'bun ',
-      BREAD: 'bread ',
-      BAGEL: 'bagel ',
-      MUFFIN: 'muffin ',
-      CROISSANT: 'croissant ',
-      BAGUETTE: 'baguette ',
-      BRIOCHE: 'brioche ',
-      CHALLAH: 'challah ',
-      SOURDOUGH: 'sourdough ',
-      NAAN: 'naan ',
-      FOCACCIA: 'focaccia ',
-      PITA: 'pita ',
-      CAKE: 'cake ',
-      PRETZEL: 'pretzel ',
-      BISCOTTI: 'biscotti ',
-      BISCUIT: 'biscuit ',
-      PANCAKE: 'pancake ',
-      SCONE: 'scone ',
-   };
-   constructor(input) {
-      this.options = input;
-      this.bread = complexSearch_c.bread_t.BREAD;
-      this.options.params.query = this.#helpCalc(this.options.params.query);
-   }
-   changeAll(input) {
-      this.options = input;
-      this.options.params.query = this.#helpCalc(this.options.params.query);
-   };
-   set query(input) {
-      this.options.params.query = this.#helpCalc(input);
-   };
-   set number(input) {
-      this.options.params.number = input;
+   static tBread = {
+     WAFFLE: 'waffle ',
+     BUN: 'bun ',
+     BREAD: 'bread ',
+     BAGEL: 'bagel ',
+     MUFFIN: 'muffin ',
+     CROISSANT: 'croissant ',
+     BAGUETTE: 'baguette ',
+     BRIOCHE: 'brioche ',
+     CHALLAH: 'challah ',
+     SOURDOUGH: 'sourdough ',
+     NAAN: 'naan ',
+     FOCACCIA: 'focaccia ',
+     PITA: 'pita ',
+     CAKE: 'cake ',
+     PRETZEL: 'pretzel ',
+     BISCOTTI: 'biscotti ',
+     BISCUIT: 'biscuit ',
+     PANCAKE: 'pancake ',
+     SCONE: 'scone '
    };
 
-   set offset(input) {
-      this.options.params.offset = input;
+   constructor (input) {
+     this.options = input
+     this.bread = cComplexSearch.bread_t.BREAD
+     this.options.params.query = this.#helpCalc(this.options.params.query)
+   }
+
+   changeAll (input) {
+     this.options = input
+     this.options.params.query = this.#helpCalc(this.options.params.query)
    };
+
+   /**
+    * @param {any} input
+    */
+   set query (input) {
+     this.options.params.query = this.#helpCalc(input)
+   };
+
+   get query () {
+     return this.options.params.query
+   }
+
+   /**
+    * @param {any} input
+    */
+   set number (input) {
+     this.options.params.number = input
+   };
+
+   get number () {
+     return this.options.params.number
+   }
+
+   /**
+    * @param {any} input
+    */
+   set offset (input) {
+     this.options.params.offset = input
+   };
+
+   get offset () {
+     return this.options.params.offset
+   }
+
    // Use when assigning this.options.params.query
-   #helpCalc(input) {
-      return (input.toLowerCase().indexOf(this.bread.substr(0, this.bread.length - 1)) >= 0) ?
-         input : this.bread + input;
+   #helpCalc (input) {
+     return (input.toLowerCase().indexOf(this.bread.substr(0, this.bread.length - 1)) >= 0)
+       ? input
+       : this.bread + input
    };
+
    /**
     * Loads recipes in data field of complexSearch_c object
     * @param {*} object complexSearch_c object
     */
-   static async complexSearch_f(object) {
-      await axios.request(object.options).then(function (response) {
-         object.data = response.data;
-      }).catch(function (error) {
-         console.log(error);
-      });
+   static async fComplexSearch (object) {
+     await axios.request(object.options).then(function (response) {
+       object.data = response.data
+     }).catch(function (error) {
+       console.log(error)
+     })
    }
 }
