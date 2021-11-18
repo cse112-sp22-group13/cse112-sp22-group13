d4ebc583b95bb3ef576d06ced66b6dd00075b0cc
Xavier Sevcik <5400808+xsevcik@users.noreply.github.com>
2021-11-18 10:59:58 -0800

    linting fixup

--- source/main.js
+++ source/main.js
@@ -2,45 +2,45 @@
 import { ComplexSearch } from '../source/apiComplexSearch.js';
 import { GenericFetch } from '../source/genericFetch.js';
 
-let initialSearch = {
-    method: 'GET',
-    url: 'https://api.spoonacular.com/recipes/complexSearch',
-    params: {
-        query: ' ', // The (natural language) recipe search query.
-        offset: 0, // The number of results to skip (between 0 and 900).
-        number: 10, //The number of expected results (between 1 and 100).
-        apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
-    }
+const initialSearch = {
+  method: 'GET',
+  url: 'https://api.spoonacular.com/recipes/complexSearch',
+  params: {
+    query: ' ', // The (natural language) recipe search query.
+    offset: 0, // The number of results to skip (between 0 and 900).
+    number: 10, // The number of expected results (between 1 and 100).
+    apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
+  }
 };
 const search = new ComplexSearch(initialSearch);
 window.addEventListener('DOMContentLoaded', init);
 
 // Initialize function, begins all of the JS code in this file
-async function init() {
-    //initializeServiceWorker();
-    //console.log("hey");
-    await ComplexSearch.fComplexSearch(search);
+async function init () {
+  // initializeServiceWorker();
+  // console.log("hey");
+  await ComplexSearch.fComplexSearch(search);
 
-    //grabbing recipes with id's
-    let bulkString = "";
-    for (let elem of search.data.results) {
-        bulkString = bulkString + elem.id + ",";
+  // grabbing recipes with id's
+  let bulkString = '';
+  for (const elem of search.data.results) {
+    bulkString = bulkString + elem.id + ',';
+  }
+
+  const bulkOptions = {
+    method: 'GET',
+    url: 'https://api.spoonacular.com/recipes/informationBulk',
+    params: {
+      ids: bulkString,
+      includeNutrition: false,
+      apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
     }
+  };
 
-    let bulkOptions = {
-        method: 'GET',
-        url: 'https://api.spoonacular.com/recipes/informationBulk',
-        params: {
-          ids: bulkString,
-          includeNutrition: false,
-          apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
-        }
-      };
-      
-    let thing = new GenericFetch(bulkOptions);
-    await GenericFetch.fGenericFetch(thing);
+  const thing = new GenericFetch(bulkOptions);
+  await GenericFetch.fGenericFetch(thing);
 
-    console.log(thing.data);
+  console.log(thing.data);
 
   /* try {
       await fetchRecipes();
@@ -70,4 +70,3 @@ async function init() {
     });
   });
 } */
-
