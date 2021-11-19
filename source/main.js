1b4072c4c6d6cfbb24c32a775a63d36a0e3f638c
davidliu <dlliu@ucsd.edu>
2021-11-18 21:43:04 -0800

    fix eslint part 2

--- source/main.js
+++ source/main.js
@@ -4,91 +4,88 @@ import { ComplexSearch } from '../source/apiComplexSearch.js';
 import { GenericFetch } from '../source/genericFetch.js';
 
 window.addEventListener('DOMContentLoaded', init);
-//LOCAL STORAGE
-let localStorage = window.localStorage; 
+// LOCAL STORAGE
+const localStorage = window.localStorage;
 
 // Initialize function, begins all of the JS code in this file
-async function init() {
-  //initializeServiceWorker();
+async function init () {
+  // initializeServiceWorker();
 
-  let initialSearch = {
+  const initialSearch = {
     method: 'GET',
     url: 'https://api.spoonacular.com/recipes/complexSearch',
     params: {
       query: ' ', // The (natural language) recipe search query.
       offset: 0, // The number of results to skip (between 0 and 900).
-      number: 20, //The number of expected results (between 1 and 100).
+      number: 20, // The number of expected results (between 1 and 100).
       apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
-      }
+    }
   };
   const search = new ComplexSearch(initialSearch);
 
   await ComplexSearch.fComplexSearch(search);
 
-  //grabbing recipes with id's
-  let idString = "";
+  // grabbing recipes with id's
+  let idString = '';
 
-  //making hash table that maps titles (key) to recipe id's (values)
+  // making hash table that maps titles (key) to recipe id's (values)
   const hashmap = new Map();
-  for(let elem of search.data.results)
-  {
+  for (const elem of search.data.results) {
     hashmap.set(elem.title, elem.id);
-    idString = idString + elem.id + ",";
+    idString = idString + elem.id + ',';
   }
-  //console.log(JSON.stringify(Array.from(hashmap.entries())));
-  //console.log(search.data.results);
+  // console.log(JSON.stringify(Array.from(hashmap.entries())));
+  // console.log(search.data.results);
 
-  let bulkOptions = {
+  const bulkOptions = {
     method: 'GET',
     url: 'https://api.spoonacular.com/recipes/informationBulk',
     params: {
       ids: idString,
       includeNutrition: false,
       apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
-      }
-    };
-      
-  let thing = new GenericFetch(bulkOptions);
+    }
+  };
+
+  const thing = new GenericFetch(bulkOptions);
   await GenericFetch.fGenericFetch(thing);
   console.log(thing.data);
 
-  //FILLING LOCAL STORAGE
-  //first set a place in local storage that will hold the hash table itself at key 0
+  // FILLING LOCAL STORAGE
+  // first set a place in local storage that will hold the hash table itself at key 0
   localStorage.setItem(0, JSON.stringify(Array.from(hashmap.entries())));
-  //extract json object and put into local storage
-  for(let elem of thing.data)
-  {
+  // extract json object and put into local storage
+  for (const elem of thing.data) {
     localStorage.setItem(elem.id, JSON.stringify(elem));
   }
 
-  //now we have local storage with the hashtable (title->id) at key 0
-  //and then the rest of local storage filled with id->json files
+  // now we have local storage with the hashtable (title->id) at key 0
+  // and then the rest of local storage filled with id->json files
 
-  //testing out searchTitle with a random title
-  let jsonObj = searchTitle(thing.data[5].title);
-  console.log("heres a json object for the title the user passed/searched:");
+  // testing out searchTitle with a random title
+  const jsonObj = searchTitle(thing.data[5].title);
+  console.log('heres a json object for the title the user passed/searched:');
   console.log(jsonObj);
 
-  // Testing searchForKey 
-  let obj = searchForKey(thing.data[0], 'title');
-  let obj2 = searchForKey(searchForKey(thing.data[1], 'analyzedInstructions'), 'steps');
-  let obj2a = getInstructionSteps(thing.data[1]);
-  let obj3 = getInstructionSteps(thing.data[2]);
+  // Testing searchForKey
+  const obj = searchForKey(thing.data[0], 'title');
+  const obj2 = searchForKey(searchForKey(thing.data[1], 'analyzedInstructions'), 'steps');
+  const obj2a = getInstructionSteps(thing.data[1]);
+  const obj3 = getInstructionSteps(thing.data[2]);
   console.log(obj);
   console.log(obj2);
   console.log(obj2a);
   console.log(obj3);
 }
 
-//take user's input for a title and returns the json object for the desired recipe
-function searchTitle(title) {
-  
-  let hashmap = new Map(JSON.parse(localStorage.getItem(0))); //grabbing that hash table
+// take user's input for a title and returns the json object for the desired recipe
+function searchTitle (title) {
+  const hashmap = new Map(JSON.parse(localStorage.getItem(0))); // grabbing that hash table
   console.log(hashmap);
-  //localStorage.getItem(key) where the key is the title/user input
-  let id = hashmap.get(title);
-  //and then query local storage using the id to get recipe
-  let jsonRecipeObj = localStorage.getItem(id);
+  // localStorage.getItem(key) where the key is the title/user input
+  const id = hashmap.get(title);
+  // and then query local storage using the id to get recipe
+  const jsonRecipeObj = localStorage.getItem(id);
   return jsonRecipeObj;
 }
 
@@ -113,6 +110,6 @@ function searchTitle(title) {
     }
   });
   return value;
-  
+
 }
 */
