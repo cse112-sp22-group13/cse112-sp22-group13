2f60232e1d1e06bdf13af8b19554573851f3a163
amerlangit <amerlan@ucsd.edu>
2021-11-18 17:11:02 -0800

    main.js editing local storage stuff

--- source/main.js
+++ source/main.js
@@ -2,71 +2,105 @@
 import { ComplexSearch } from '../source/apiComplexSearch.js';
 import { GenericFetch } from '../source/genericFetch.js';
 
-const initialSearch = {
-  method: 'GET',
-  url: 'https://api.spoonacular.com/recipes/complexSearch',
-  params: {
-    query: ' ', // The (natural language) recipe search query.
-    offset: 0, // The number of results to skip (between 0 and 900).
-    number: 10, // The number of expected results (between 1 and 100).
-    apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
-  }
-};
-const search = new ComplexSearch(initialSearch);
 window.addEventListener('DOMContentLoaded', init);
+//LOCAL STORAGE
+let localStorage = window.localStorage; 
 
 // Initialize function, begins all of the JS code in this file
-async function init () {
-  // initializeServiceWorker();
-  // console.log("hey");
+async function init() {
+  //initializeServiceWorker();
+
+  let initialSearch = {
+    method: 'GET',
+    url: 'https://api.spoonacular.com/recipes/complexSearch',
+    params: {
+      query: ' ', // The (natural language) recipe search query.
+      offset: 0, // The number of results to skip (between 0 and 900).
+      number: 20, //The number of expected results (between 1 and 100).
+      apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
+      }
+  };
+  const search = new ComplexSearch(initialSearch);
+
   await ComplexSearch.fComplexSearch(search);
 
-  // grabbing recipes with id's
-  let bulkString = '';
-  for (const elem of search.data.results) {
-    bulkString = bulkString + elem.id + ',';
-  }
+  //grabbing recipes with id's
+  let idString = "";
 
-  const bulkOptions = {
+  //making hash table that maps titles (key) to recipe id's (values)
+  const hashmap = new Map();
+  for(let elem of search.data.results)
+  {
+    hashmap.set(elem.title, elem.id);
+    idString = idString + elem.id + ",";
+  }
+  //console.log(JSON.stringify(Array.from(hashmap.entries())));
+  //console.log(search.data.results);
+
+  let bulkOptions = {
     method: 'GET',
     url: 'https://api.spoonacular.com/recipes/informationBulk',
     params: {
-      ids: bulkString,
+      ids: idString,
       includeNutrition: false,
       apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
-    }
-  };
-
-  const thing = new GenericFetch(bulkOptions);
+      }
+    };
+      
+  let thing = new GenericFetch(bulkOptions);
   await GenericFetch.fGenericFetch(thing);
-
   console.log(thing.data);
 
-  /* try {
-      await fetchRecipes();
-    } catch (err) {
-      console.log(`Error fetching recipes: ${err}`);
-      return;
-    } */
+  //FILLING LOCAL STORAGE
+  //first set a place in local storage that will hold the hash table itself at key 0
+  localStorage.setItem(0, JSON.stringify(Array.from(hashmap.entries())));
+  //extract json object and put into local storage
+  for(let elem of thing.data)
+  {
+    localStorage.setItem(elem.id, JSON.stringify(elem));
+  }
+
+  //now we have local storage with the hashtable (title->id) at key 0
+  //and then the rest of local storage filled with id->json files
+
+  //testing out searchTitle
+  let jsonObj = searchTitle(thing.data[5].title);
+  console.log("heres a json object for the title the user passed/searched:");
+  console.log(jsonObj);
 }
 
-/* async function fetchRecipes() {
-  return new Promise((resolve, reject) => {
-    recipes.forEach(recipe => {
-      fetch(recipe)
-        .then(response => response.json())
-        .then(data => {
-          // This grabs the page name from the URL in the array above
-          data['page-name'] = recipe.split('/').pop().split('.')[0];
-          recipeData[recipe] = data;
-          if (Object.keys(recipeData).length == recipes.length) {
-            resolve();
-          }
-        })
-        .catch(err => {
-          console.log(`Error loading the ${recipe} recipe`);
-          reject(err);
-        });
-    });
+function searchTitle(title) {
+  
+  let hashmap = new Map(JSON.parse(localStorage.getItem(0))); //grabbing that hash table
+  console.log(hashmap);
+  //localStorage.getItem(key) where the key is the title/user input
+  let id = hashmap.get(title);
+  //and then query local storage using the id to get recipe
+  let jsonRecipeObj = localStorage.getItem(id);
+  return jsonRecipeObj;
+}
+
+/** LAB6 SEARCH KEY
+ * Recursively search for a key nested somewhere inside an object
+ * @param {Object} object the object with which you'd like to search
+ * @param {String} key the key that you are looking for in the object
+ * @returns {*} the value of the found key
+ */
+/*
+ function searchForKey(object, key) {
+
+  var value;
+  Object.keys(object).some(function (k) {
+    if (k === key) {
+      value = object[k];
+      return true;
+    }
+    if (object[k] && typeof object[k] === 'object') {
+      value = searchForKey(object[k], key);
+      return value !== undefined;
+    }
   });
-} */
+  return value;
+  
+}
+*/
