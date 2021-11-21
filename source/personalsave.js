110325906d5dcc42cf80d6ea7c85810048b4906f
GitPat-Sudo <p1wang@ucsd.edu>
2021-11-20 17:46:14 -0800

    linte

--- source/personalsave.js
+++ source/personalsave.js
@@ -1,54 +1,52 @@
-//should recieve a website  url
-//import {extraction, forceExtraction} from '../source/personalsave.js'; in main
-//var x = extraction('https://foodista.com/recipe/ZHK4KPB6/chocolate-crinkle-cookies'); to try it out in main
+// should recieve a website url
+// import {extraction, forceExtraction} from '../source/personalsave.js'; in main
+// var x = extraction('https://foodista.com/recipe/ZHK4KPB6/chocolate-crinkle-cookies'); to try it out in main
 export async function extraction(input){
-    var data = {}
-    console.log('using');
-    const format = {
-        method: 'GET',
-        url: 'https://api.spoonacular.com/recipes/extract',
-        params: {
-          url: input,
-          forceExtraction: false,
-          analyze: false,
-          includeNutrition: false,
-          includeTaste: false,
-          apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
-        }
-      };
-    await axios.request(format).then(function(response){
-        data = response.data
-        console.log(JSON.stringify(data));
-        return data;
-    }).catch(function(error) {
-        console.log('shiiet');
-        console.log(error);
-    });
- 
+  var data = {};
+  console.log('using');
+  const format = {
+    method: 'GET',
+    url: 'https://api.spoonacular.com/recipes/extract',
+    params: {
+      url: input,
+      forceExtraction: false,
+      analyze: false,
+      includeNutrition: false,
+      includeTaste: false,
+      apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
+      }
+    };
+  await axios.request(format).then(function(response){
+    data = response.data
+    console.log(JSON.stringify(data));
+    return data;
+  }).catch(function(error) {
+    console.log('shiiet');
+    console.log(error);
+  });
 }
  
-export async function forceExtraction(input){
-    var data = {}
-    console.log('using');
-    const format = {
-        method: 'GET',
-        url: 'https://api.spoonacular.com/recipes/extract',
-        params: {
-          url: input,
-          forceExtraction: true,
-          analyze: false,
-          includeNutrition: false,
-          includeTaste: false,
-          apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
-        }
-      };
-    await axios.request(format).then(function(response){
-        data = response.data
-        console.log(JSON.stringify(data));
-        return data;
-    }).catch(function(error) {
-        console.log('shiiet');
-        console.log(error);
-    });
- 
+export async function extraction(input){
+  var data = {};
+  console.log('using');
+  const format = {
+    method: 'GET',
+    url: 'https://api.spoonacular.com/recipes/extract',
+    params: {
+      url: input,
+      forceExtraction: true,
+      analyze: false,
+      includeNutrition: false,
+      includeTaste: false,
+      apiKey: 'a6e411c0c3e349d29672f54d7ba122e3'
+      }
+    };
+  await axios.request(format).then(function(response){
+    data = response.data
+    console.log(JSON.stringify(data));
+    return data;
+  }).catch(function(error) {
+    console.log('shiiet');
+    console.log(error);
+  });
 }
