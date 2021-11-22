6061be018a2499b4115a81df8db5905e1afcaccb
Xavier Sevcik <5400808+xsevcik@users.noreply.github.com>
2021-11-22 13:30:05 -0800

    lint `sideBar.js`

--- src/scripts/sideBar.js
+++ src/scripts/sideBar.js
@@ -1,9 +1,9 @@
-function openNav() {
-    document.querySelector("#sideNavbar").style.width = "250px";
-    document.querySelector("#navButton .openbtn").onclick = closeNav;
+function openNav () {
+  document.querySelector('#sideNavbar').style.width = '250px';
+  document.querySelector('#navButton .openbtn').onclick = closeNav;
 }
 
-function closeNav() {
-    document.querySelector("#sideNavbar").style.width = "0";
-    document.querySelector("#navButton .openbtn").onclick = openNav;
+function closeNav () {
+  document.querySelector('#sideNavbar').style.width = '0';
+  document.querySelector('#navButton .openbtn').onclick = openNav;
 }
