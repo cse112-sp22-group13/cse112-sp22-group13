e3527d8089250a57982180d1d62f93ae9948b978
PatriciaWuu <lbdqjj@gmail.com>
2021-11-24 18:19:23 -0800

    change doc

--- src/scripts/FavoriteRecipe.js
+++ src/scripts/FavoriteRecipe.js
@@ -1,7 +1,7 @@
   /**
-   * *************************GETTAGS FUNCTION************************* *
-   * Function that will build an array containing title, ingredients,   *
-   * and tags for true booleans within the json file passed             *
+   * *************************markFav FUNCTION************************* *
+   * Mark favorite as true in hashmap by id of recipe, store the        *
+   * hashmap in localStorage                                            *
    * ****************************************************************** *
    */
    function markFav(id){
@@ -12,13 +12,14 @@
    }
  
     /**
-    * *************************GETTAGS FUNCTION************************* *
-    * Function that will build an array containing title, ingredients,   *
-    * and tags for true booleans within the json file passed             *
+    * *************************unFav FUNCTION************************* *
+    * Mark favorite as false in hashmap by id of recipe, replace the     *
+    * hashmap in localStorage                                           *
     * ****************************************************************** *
     */
     function unFav(id){
       // get hash table
+      
       const favmap = JSON.parse(localStorage['2']);
       favmap.set(id, false);
       localStorage.setItem(2, JSON.stringify(Array.from(favmap.entries())));
