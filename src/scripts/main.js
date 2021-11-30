578b2aebb006ec29ca93b8dea690d7bff7b495c2
amerlangit <amerlan@ucsd.edu>
2021-11-29 17:35:26 -0800

    only fetch/fill local storage when empty

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -27,93 +27,98 @@ const localStorage = window.localStorage;
   * *************************************************************** *
   */
 async function init () {
+
   // initializeServiceWorker(); will eventually implement
 
-  const initialSearch = {
-    method: 'GET',
-    url: 'https://api.spoonacular.com/recipes/complexSearch',
-    params: {
-      query: ' ', // The (natural language) recipe search query.
-      offset: 0, // The number of results to skip (between 0 and 900).
-      number: 20, // The number of expected results (between 1 and 100).
-      apiKey: API_KEY
-    }
-  };
-
-  const search = new ComplexSearch(initialSearch);
-  await ComplexSearch.fComplexSearch(search);
-  // console.log(search.data);
-
-  // grabbing recipes with id's
-  let idString = '';
-
-  // making hash table that maps titles (key) to recipe id's (values)
-  const hashmap = new Map();
-  for (const elem of search.data.results) {
-    hashmap.set(elem.title, elem.id);
-    idString = idString + elem.id + ',';
-  }
-
-  // SANAT
-  const objSanat = {
-    analyzedInstructions: [{ name: '', steps: [] }],
-    title: 'Sanat',
-    id : 1,
-    image: 'https://avatars.githubusercontent.com/u/31770675?v=4',
-    extendedIngredients: [{ original: 'naan bread' }, { original: 'spices' }, { original: 'hot dog' }],
-    cheap: true,
-    dairyFree: false,
-    glutenFree: false,
-    vegan: false,
-    vegetarian: false,
-    healthy: false
-  };
-  hashmap.set(objSanat.title, 1);
-  localStorage.setItem(1, JSON.stringify(objSanat));
-
-  // console.log(JSON.stringify(Array.from(hashmap.entries())));
-  // console.log(search.data.results);
-
-  const bulkOptions = {
-    method: 'GET',
-    url: 'https://api.spoonacular.com/recipes/informationBulk',
-    params: {
-      ids: idString,
-      includeNutrition: false,
-      apiKey: API_KEY
-    }
-  };
-
-  const thing = new GenericFetch(bulkOptions);
-  await GenericFetch.fGenericFetch(thing);
-  console.log(thing.data);
-
-  // FILLING LOCAL STORAGE
-  // first set a place in local storage that will hold the hash table itself at key 0
-  localStorage.setItem(0, JSON.stringify(Array.from(hashmap.entries())));
-  // extract json object and put into local storage
-  for (const elem of thing.data) {
-    localStorage.setItem(elem.id, JSON.stringify(elem));
-  }
-  console.log("we are here");
-
-  // MAKING FAVORITES HASHMAP THAT WILL BE LOCATED AT #2 IN LOCAL STORAGE
-  const favmap = new Map();
-  // MAKING DELETES HASHMAP THAT WILL BE LOCATED AT #3 IN LOCAL STORAGE
-  const deletedMap = new Map();
-  // get hash table
-  const hashes = JSON.parse(localStorage['0']);
-  // get array of ids
-  const elementIdArr = hashes.map(h => h[1]);
+  if(localStorage.length == 0)
+  {
+    const initialSearch = {
+      method: 'GET',
+      url: 'https://api.spoonacular.com/recipes/complexSearch',
+      params: {
+        query: ' ', // The (natural language) recipe search query.
+        offset: 0, // The number of results to skip (between 0 and 900).
+        number: 20, // The number of expected results (between 1 and 100).
+        apiKey: API_KEY
+      }
+    };
   
-  for (let i = 0; i < elementIdArr.length; i++) {
-    // initialze every id as false (not yet a favorite or deleted)
-    favmap.set(elementIdArr[i], false);
-    deletedMap.set(elementIdArr[i], false);
+    const search = new ComplexSearch(initialSearch);
+    await ComplexSearch.fComplexSearch(search);
+    // console.log(search.data);
+  
+    // grabbing recipes with id's
+    let idString = '';
+  
+    // making hash table that maps titles (key) to recipe id's (values)
+    const hashmap = new Map();
+    for (const elem of search.data.results) {
+      hashmap.set(elem.title, elem.id);
+      idString = idString + elem.id + ',';
+    }
+  
+    // SANAT
+    const objSanat = {
+      analyzedInstructions: [{ name: '', steps: [] }],
+      title: 'Sanat',
+      id : 1,
+      image: 'https://avatars.githubusercontent.com/u/31770675?v=4',
+      extendedIngredients: [{ original: 'naan bread' }, { original: 'spices' }, { original: 'hot dog' }],
+      cheap: true,
+      dairyFree: false,
+      glutenFree: false,
+      vegan: false,
+      vegetarian: false,
+      healthy: false
+    };
+    hashmap.set(objSanat.title, 1);
+    localStorage.setItem(1, JSON.stringify(objSanat));
+  
+    // console.log(JSON.stringify(Array.from(hashmap.entries())));
+    // console.log(search.data.results);
+  
+    const bulkOptions = {
+      method: 'GET',
+      url: 'https://api.spoonacular.com/recipes/informationBulk',
+      params: {
+        ids: idString,
+        includeNutrition: false,
+        apiKey: API_KEY
+      }
+    };
+  
+    const thing = new GenericFetch(bulkOptions);
+    await GenericFetch.fGenericFetch(thing);
+    console.log(thing.data);
+  
+    // FILLING LOCAL STORAGE
+    // first set a place in local storage that will hold the hash table itself at key 0
+    localStorage.setItem(0, JSON.stringify(Array.from(hashmap.entries())));
+    // extract json object and put into local storage
+    for (const elem of thing.data) {
+      localStorage.setItem(elem.id, JSON.stringify(elem));
+    }
+    console.log("we are here");
+  
+    // MAKING FAVORITES HASHMAP THAT WILL BE LOCATED AT #2 IN LOCAL STORAGE
+    const favmap = new Map();
+    // MAKING DELETES HASHMAP THAT WILL BE LOCATED AT #3 IN LOCAL STORAGE
+    const deletedMap = new Map();
+    // get hash table
+    const hashes = JSON.parse(localStorage['0']);
+    // get array of ids
+    const elementIdArr = hashes.map(h => h[1]);
+    
+    for (let i = 0; i < elementIdArr.length; i++) {
+      // initialze every id as false (not yet a favorite or deleted)
+      favmap.set(elementIdArr[i], false);
+      deletedMap.set(elementIdArr[i], false);
+    }
+    // store the fav and del maps in localstorage
+    localStorage.setItem(2, JSON.stringify(Array.from(favmap.entries())));
+    localStorage.setItem(3, JSON.stringify(Array.from(deletedMap.entries())));
+  
+    console.log('local storage has ', localStorage.length, ' elements');
   }
-  // store the fav and del maps in localstorage
-  localStorage.setItem(2, JSON.stringify(Array.from(favmap.entries())));
-  localStorage.setItem(3, JSON.stringify(Array.from(deletedMap.entries())));
 
-  console.log('local storage has ', localStorage.length, ' elements');
 }
