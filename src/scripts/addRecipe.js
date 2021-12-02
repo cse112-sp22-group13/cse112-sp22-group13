df34c95761aac59e046b6326a55737bb6c4e9673
amerlangit <amerlan@ucsd.edu>
2021-12-01 21:23:32 -0800

    fix some bugs with added recipe

--- src/scripts/addRecipe.js
+++ src/scripts/addRecipe.js
@@ -62,8 +62,9 @@ async function forceExtraction (input) {
 async function addRecipe () {
   const inputData = inputHTML.value;
 
-  // add recipe onlt if not duplicated
-  if (checkckDu(inputData)) {
+  // BASE CASES PRIOR TO CONTINUING TO ALTER LOCAL STORAGE:
+  // add recipe only if not duplicated
+  if (checkDup(inputData)) {
     alert('Duplicated recipe.');
     return;
   }
@@ -73,39 +74,46 @@ async function addRecipe () {
     alert('Not a valid url');
     return;
   }
-  const recipeData = JSON.stringify(recipetoHash);
+
+  // GOOD TO GO!
+  // grab json file, and begin creating a recipe card with it
+  console.log('heres the json of your added recipe:');
+  console.log(recipetoHash);
   const main = document.querySelector('main');
   const element = document.createElement('recipe-card');
 
-  // generate the vlaid id
-  const validID = Math.random() * 1000;
+  // generate a random valid id because it's added with an id of -1
+  const validID = Math.floor(Math.random() * 1000);
 
-  // grab value from localStorage
+  // grab maps from localStorage for insertion and replacement
   const hashMap = new Map(JSON.parse(localStorage['0']));
   const favMap = new Map(JSON.parse(localStorage['2']));
-  const DelMap = new Map(JSON.parse(localStorage['3']));
+  const delMap = new Map(JSON.parse(localStorage['3']));
   const urlMap = new Map(JSON.parse(localStorage['5']));
 
   // set value in map for new added card
   hashMap.set(recipetoHash.title, validID);
   favMap.set(validID, false);
-  DelMap.set(validID, false);
+  delMap.set(validID, false);
   console.log(recipetoHash.sourceUrl);
 
   // urlmap's value is for store id to check for dulipated.
   urlMap.set(recipetoHash.sourceUrl, validID);
+  // also edit the inner ID of json file
+  recipetoHash.id = validID;
+  const recipeData = JSON.stringify(recipetoHash);
 
   // update local storage
   localStorage.setItem(validID, recipeData);
   localStorage.setItem(0, JSON.stringify(Array.from(hashMap.entries())));
   localStorage.setItem(2, JSON.stringify(Array.from(favMap.entries())));
-  localStorage.setItem(3, JSON.stringify(Array.from(DelMap.entries())));
+  localStorage.setItem(3, JSON.stringify(Array.from(delMap.entries())));
   localStorage.setItem(5, JSON.stringify(Array.from(urlMap.entries())));
 
-  // updated recipe card  in html
+  // update recipe card in html
   element.data = recipeData;
   element.id = validID;
-  if (DelMap.get(element.id) === true) {
+  if (delMap.get(element.id) === true) {
     element.classList.add('deleted');
   }
   main.appendChild(element);
@@ -120,20 +128,20 @@ async function addRecipe () {
 
 /**
  * Check if the recipe is already in the localStorage
- * @param {String} urll url which comes from user input
+ * @param {String} url url which comes from user input
  * @returns {Boolean} true if there exist, false if not.
  */
-function checkckDu (urll) {
+function checkDup (url) {
   const delHash = new Map(JSON.parse(localStorage['3']));
   const urlHash = new Map(JSON.parse(localStorage['5']));
   // if url is empty, means no url is added before, return false
-  if (!urlHash.has(urll)) {
+  if (!urlHash.has(url)) {
     console.log('false');
     return false;
   }
   // or url is not empty, deleteMap is true, meaning the added recipe has been deleted, return false
-  if (delHash.get(urlHash.get(urll)) === true) {
-    console.log(urlHash.get(urll));
+  if (delHash.get(urlHash.get(url)) === true) {
+    console.log(urlHash.get(url));
     return false;
   }
   return true;
