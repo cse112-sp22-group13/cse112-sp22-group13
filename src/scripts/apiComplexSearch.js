0a1e53ede42a3bbe3dbfda5bc0745338c3bcafdc
Andrija Sunjic <51809572+andrija-s@users.noreply.github.com>
2021-12-02 16:35:28 -0800

    comment additions/fixes

--- src/scripts/apiComplexSearch.js
+++ src/scripts/apiComplexSearch.js
@@ -1,6 +1,7 @@
 // put "import { ComplexSearch } from './apiComplexSearch.js';" in main.js without quotes
 /**
- * @param {string} input
+ * Object to perform make axios fetch for recipe information (images, ids).
+ * @param {string} input Options object holding parameters for fetch call
  *
  */
 export class ComplexSearch {
@@ -32,52 +33,61 @@ export class ComplexSearch {
      this.bread = ComplexSearch.tBread.BREAD;
      this.options.params.query = this.helpCalc(this.options.params.query);
    }
-
+   /**
+    * Change all the options parameters by passing in new options object
+    * @param {Object} input Object holding options parameters for fetch call
+    */
    changeAll (input) {
      this.options = input;
      this.options.params.query = this.helpCalc(this.options.params.query);
    };
-
    /**
-    * @param {any} input
+    * Changing the query parameter
+    * @param {String} input New search query
     */
    set query (input) {
      this.options.params.query = this.helpCalc(input);
    };
-
+   /**
+    * Get the query parameter
+    */
    get query () {
      return this.options.params.query;
    }
-
    /**
-    * @param {any} input
+    * Changing the number parameter
+    * @param {String} new number
     */
    set number (input) {
      this.options.params.number = input;
    };
-
+   /**
+    * Get the number parameter
+    */
    get number () {
      return this.options.params.number;
    }
-
    /**
-    * @param {any} input
+    * Set the offset parameter
+    * @param {String} new offset
     */
    set offset (input) {
      this.options.params.offset = input;
    };
-
+   /**
+    * Get the offset parameter
+    */
    get offset () {
      return this.options.params.offset;
    }
-
-   // Use when assigning this.options.params.query
+   /**
+    * Use when assigning this.options.params.query to prepend a bread noun to force a bread search in this bread universe
+    */
    helpCalc (input) {
      return (input.toLowerCase().indexOf(this.bread.substr(0, this.bread.length - 1)) >= 0)
        ? input
        : this.bread + input;
    };
-
    /**
     * Loads recipes in data field of complexSearch_c object
     * @param {*} object complexSearch_c object
