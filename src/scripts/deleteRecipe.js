ae90fe42b71e3e7cd8d9d1e4019f428826d58e4a
Sanat Bhandarkar <sanatbhandarkar@Sanats-MacBook-Pro-2.local>
2021-12-12 15:21:03 -0800

    Testing Build

--- src/scripts/deleteRecipe.js
+++ src/scripts/deleteRecipe.js
@@ -4,7 +4,7 @@
  * @param {int} id , Id of Recipe to Delete
  */
 export function deleteRecipe (id) {
-  // get hash table
+  // get the hash table
   const deletedMap = new Map(JSON.parse(localStorage['3']));
   deletedMap.set(parseInt(id), true);
   localStorage.setItem(3, JSON.stringify(Array.from(deletedMap.entries())));
