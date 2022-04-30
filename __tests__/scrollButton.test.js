7fa446361a725a0ed8d6f8f43c1f0d1f03f093a4
xsevcik <5400808+xsevcik@users.noreply.github.com>
2022-04-29 23:04:28 -0700

    Upgrade testing system to use puppeteer on top of jest

--- __tests__/scrollButton.test.js
+++
@@ -1,7 +0,0 @@
-const functions = require("../src/scripts/scrollButton.js");
-
-test("Checking the functionality of the scroll to top button", () => {
-    functions.topFunction();
-    expect(document.body.scrollTop).toStrictEqual(0);
-    expect(document.documentElement.scrollTop).toStrictEqual(0);
-});
