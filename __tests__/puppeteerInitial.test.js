f22b2e1bd564721038144a11eddcb5680452ed94
esong165 <eysong@ucsd.edu>
2022-05-07 01:10:09 -0700

    Very simple Jest-Puppeteer Server Test

--- __tests__/puppeteerInitial.test.js
+++ __tests__/puppeteerInitial.test.js
@@ -1,10 +1,55 @@
+// const server = require("../server");
+const puppeteer = require("puppeteer");
+const http = require("http");
+const fs = require("fs").promises;
+
 describe("Knead It", () => {
     beforeAll(async () => {
-        console.log(process.cwd());
-        await page.goto("file://" + process.cwd() + "/src/home/home.html");
+        // console.log(process.cwd());
+        // await page.goto("file://" + process.cwd() + "/src/home/home.html");
+        const host = "localhost";
+        const port = 8000;
+
+        let htmlFile;
+        let cssFile;
+
+        const requestListener = function (req, res) {
+            switch (req.url) {
+            case "/home/home.css":
+                res.setHeader("Content-Type", "text/css");
+                res.writeHead(200);
+                res.write(cssFile);
+                break;
+            default:
+                res.setHeader("Content-Type", "text/html");
+                res.writeHead(200);
+                res.write(htmlFile);
+        
+            }
+            res.end();
+    
+        };
+
+        fs.readFile("src/home/home.html").then(contents => {
+            htmlFile = contents;
+        });
+        fs.readFile("src/home/home.css").then(contents => {
+            cssFile = contents;
+        });
+
+
+        const server = http.createServer(requestListener);
+        server.listen(port, host, () => {
+            // console.log(`Server is running on http://${host}:${port}`);
+        });
+        const browser = await puppeteer.launch();
     });
 
     it("should be titled \"KNEAD IT\"", async () => {
+        const page = await browser.newPage();
+        await page.goto("http://localhost:8000");
+
         await expect(page.title()).resolves.toMatch("KNEAD IT");
     });
+
 });
