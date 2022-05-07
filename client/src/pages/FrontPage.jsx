82a46f4619623195d9b7fa69415f7d935764ee4f
Anh Pham <anh65498@gmail.com>
2022-05-07 15:30:28 -0700

    clean up client/public folder

---
+++ client/src/pages/FrontPage.jsx
@@ -0,0 +1,20 @@
+import React, {Fragment} from "react";
+import ScrollToTop from "../components/ScrollToTop";
+
+const FrontPage = () => {
+
+    const mockData = [{
+        "name": "",
+        "photo": 
+    }]
+    return (
+        <Fragment>
+            <h2>CUISINE</h2>
+            <h2>PROTEIN</h2>
+            <h2>PREP TIME</h2>
+        </Fragment>
+    )
+}
+
+
+export default FrontPage;
