3174d954770309fe164a0ac32d73440d4dc72904
Anh Pham <anh65498@gmail.com>
2022-05-09 14:59:05 -0700

    WIP of Recipes Page

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,7 +1,6 @@
 import React, { Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
 
-import FrontPage from "../pages/FrontPage";
 import RecipesPage from "../pages/RecipesPage";
 
 
@@ -9,7 +8,6 @@ const Main = () => {
   return (
     <Fragment>
       <Switch>
-        <Route exact path="/" render={FrontPage} />
         <Route exact path="/recipes" render={RecipesPage} />
       </Switch>
     </Fragment>
