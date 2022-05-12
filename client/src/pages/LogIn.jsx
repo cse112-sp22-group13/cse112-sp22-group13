6e8c87f8b0916e2a03b53fe2aeea5a35655ab5eb
Anh Pham <anh65498@gmail.com>
2022-05-11 23:48:24 -0700

    Fix React warnings in console

--- client/src/pages/LogIn.jsx
+++ client/src/pages/LogIn.jsx
@@ -5,52 +5,52 @@ const LogInPage = () => {
   return (
     <Fragment>
       <div
-        class="modal position-static d-block bg-secondary py-3"
-        tabindex="-1"
+        className="modal position-static d-block bg-secondary py-3"
+        tabIndex="-1"
         role="dialog"
         id="modalSignin"
       >
-        <div class="modal-dialog" role="document">
-          <div class="modal-content rounded-5 shadow">
-            <div class="modal-header p-5 pb-4 border-bottom-0">
-              <h2 class="fw-bold mb-0 mx-auto">Log In</h2>
+        <div className="modal-dialog" role="document">
+          <div className="modal-content rounded-5 shadow">
+            <div className="modal-header p-5 pb-4 border-bottom-0">
+              <h2 className="fw-bold mb-0 mx-auto">Log In</h2>
             </div>
 
-            <div class="modal-body p-5 pt-0">
-              <form class="">
-                <div class="form-floating mb-3">
+            <div className="modal-body p-5 pt-0">
+              <form className="">
+                <div className="form-floating mb-3">
                   <input
                     type="email"
-                    class="form-control rounded-4"
+                    className="form-control rounded-4"
                     id="floatingInput"
                     placeholder="name@example.com"
                   />
-                  <label for="floatingInput">Email address</label>
+                  <label htmlFor="floatingInput">Email address</label>
                 </div>
-                <div class="form-floating mb-3">
+                <div className="form-floating mb-3">
                   <input
                     type="password"
-                    class="form-control rounded-4"
+                    className="form-control rounded-4"
                     id="floatingPassword"
                     placeholder="Password"
                   />
-                  <label for="floatingPassword">Password</label>
+                  <label htmlFor="floatingPassword">Password</label>
                 </div>
                 <button
-                  class="w-100 mb-2 btn btn-lg rounded-4 btn-warning"
+                  className="w-100 mb-2 btn btn-lg rounded-4 btn-warning"
                   type="submit"
                 >
                   Log In
                 </button>
 
-                <div class="d-flex flex-row align-items-center py-2">
+                <div className="d-flex flex-row align-items-center py-2">
                   <hr className="w-50" />
-                  <p class="my-auto">OR</p>
+                  <p className="my-auto">OR</p>
                   <hr className="w-50" />
                 </div>
 
                 <button
-                  class="w-100 py-2 mb-2 btn btn-danger rounded-4"
+                  className="w-100 py-2 mb-2 btn btn-danger rounded-4"
                   type="submit"
                 >
                   <svg
@@ -58,7 +58,7 @@ const LogInPage = () => {
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-google mx-2"
+                    className="bi bi-google mx-2"
                     viewBox="0 0 16 16"
                   >
                     <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
@@ -66,7 +66,7 @@ const LogInPage = () => {
                   Log In with Google
                 </button>
                 <button
-                  class="w-100 py-2 mb-2 btn btn-primary rounded-4"
+                  className="w-100 py-2 mb-2 btn btn-primary rounded-4"
                   type="submit"
                 >
                   <svg
@@ -74,7 +74,7 @@ const LogInPage = () => {
                     width="16"
                     height="16"
                     fill="currentColor"
-                    class="bi bi-facebook mx-2"
+                    className="bi bi-facebook mx-2"
                     viewBox="0 0 16 16"
                   >
                     <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
@@ -82,9 +82,11 @@ const LogInPage = () => {
                   Log In with Facebook
                 </button>
               </form>
-              <small class="text-muted mx-auto mt-5">Not a User?</small>
+              <small className="text-muted mx-auto mt-5">Not a User?</small>
               <Link to="/signup" className="link">
-                <button class="w-100 my-2 btn btn-lg btn-info">Sign Up</button>
+                <button className="w-100 my-2 btn btn-lg btn-info">
+                  Sign Up
+                </button>
               </Link>
             </div>
           </div>
