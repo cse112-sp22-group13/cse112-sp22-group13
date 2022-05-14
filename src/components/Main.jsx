4539fa3308e76759d8e9ca7b0de595b2733c3e1d
xsevcik <5400808+xsevcik@users.noreply.github.com>
2022-05-14 13:51:02 -0700

    Move code from `client` directory to root of project

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,16 +4,15 @@ import { Route, Switch } from "react-router-dom";
 import FrontPage from "../pages/FrontPage";
 import RecipesPage from "../pages/RecipesPage";
 
-
 const Main = () => {
-  return (
-    <Fragment>
-      <Switch>
-        <Route exact path="/" render={FrontPage} />
-        <Route exact path="/recipes" render={RecipesPage} />
-      </Switch>
-    </Fragment>
-  );
+    return (
+        <Fragment>
+            <Switch>
+                <Route exact path="/" render={FrontPage} />
+                <Route exact path="/recipes" render={RecipesPage} />
+            </Switch>
+        </Fragment>
+    );
 };
 
 export default Main;
