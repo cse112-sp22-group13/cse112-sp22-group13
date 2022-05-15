c38d760cd9a07b1164fe89ba3186de7d56306034
Anh Pham <anh65498@gmail.com>
2022-05-15 15:52:56 -0700

    Link navbar.css to NavBar.jsx

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,17 +1,14 @@
 import React, { Fragment } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import FrontPage from "../pages/FrontPage";
-import RecipesPage from "../pages/RecipesPage";
-
 
 const Main = () => {
   return (
     <Fragment>
-      <Switch>
-        <Route exact path="/" render={FrontPage} />
-        <Route exact path="/" render={RecipesPage} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<FrontPage />} />
+      </Routes>
     </Fragment>
   );
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,35 +1,56 @@
-import React, { Fragment } from "react";
-import '../stylesheets/NavBar.css';
+import React from "react";
+import "../stylesheets/navbar.css";
 //import { Route, Switch } from "react-router-dom";
 
 //import FrontPage from "../pages/FrontPage";
 //import RecipesPage from "../pages/RecipesPage";
 
-const navbar =  () => {
-    return (
+const NavBar = () => {
+  return (
     <div class="container">
-    <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-      <img src="logo192.png" alt="Logo" width="75" height="75"></img>
-      </a>
+      <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+        <a
+          href="/"
+          class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+        >
+          <img src="logo192.png" alt="Logo" width="75" height="75"></img>
+        </a>
 
-      <form id="form_search" name="form_search" method="get" action="" class="form-inline">
-        <a href="#" class="nav-link px-2 link-secondary">Knead It</a>
-        <div class="input-group">
-            <input class="form-control" placeholder="Search..." type="text"/>
+        <form
+          id="form_search"
+          name="form_search"
+          method="get"
+          action=""
+          class="form-inline"
+        >
+          <a href="#" class="nav-link px-2 link-secondary">
+            Knead It
+          </a>
+          <div class="input-group">
+            <input class="form-control" placeholder="Search..." type="text" />
             <span class="input-group-btn">
-                <button class="btn btn-outline-primary me-2" type="button">Search</button>
+              <button
+                id="test"
+                class="btn btn-outline-primary me-2"
+                type="button"
+              >
+                Search
+              </button>
             </span>
+          </div>
+        </form>
+
+        <div class="col-md-3 text-end">
+          <button type="button" class="btn btn-outline-primary me-2">
+            Login
+          </button>
+          <button type="button" class="btn btn-primary">
+            Sign-up
+          </button>
         </div>
-      </form>
+      </header>
+    </div>
+  );
+};
 
-      <div class="col-md-3 text-end">
-        <button type="button" class="btn btn-outline-primary me-2">Login</button>
-        <button type="button" class="btn btn-primary">Sign-up</button>
-      </div>
-      
-    </header>
-  </div>
-)}
-
-export default navbar;
+export default NavBar;
