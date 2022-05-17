d7d029539797ec084bf3845b3b72e6a62a27a0d4
Anh Pham <anh65498@gmail.com>
2022-05-16 21:55:38 -0700

    WIP. Sending to Xavier to clean up client

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import NavBar from "./components/NavBar";
+import NavBar from "./components/Navbar";
 import Main from "./components/Main";
 
 const App = (props) => (
