9ab7f13ef9d81fa57a8327cbdb5f28bed12c2205
Sinemurian <lweather@ucsd.edu>
2022-05-23 15:39:45 -0700

    added timer icons and fixed checkbox issue

--- src/pages/FrontPage.jsx
+++ src/pages/FrontPage.jsx
@@ -2,6 +2,13 @@ import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import MockPhoto from "../media/mock-photo.jpg";
 import "../stylesheets/frontpage.css";
+import timer5 from "../media/timer5.png";
+import timer10 from "../media/timer10.png";
+import timer15 from "../media/timer15.png";
+import timer30 from "../media/timer30.png";
+import timer45 from "../media/timer45.png";
+import timer60 from "../media/timer60.png";
+import timer2hr from "../media/timer2hr.png";
 
 const FrontPage = () => {
     // TODO: replace it with Spoonacular API response?
@@ -85,6 +92,36 @@ const FrontPage = () => {
         "1 hour",
         "2 hour"
     ];
+    const prepTimeImg = [
+        {
+            name: "5 min",
+            img: timer5
+        },
+        {
+            name: "10 min",
+            img: timer10
+        },
+        {
+            name: "15 min",
+            img: timer15
+        },
+        {
+            name: "30 min",
+            img: timer30
+        },
+        {
+            name: "45 min",
+            img: timer45
+        },
+        {
+            name: "1 hr",
+            img: timer60
+        },
+        {
+            name: "2 hr",
+            img: timer2hr
+        },
+    ];
 
     return (
         <Fragment>
@@ -111,7 +148,7 @@ const FrontPage = () => {
             </div>
             <h4>PREP TIME</h4>
             <div className="scrolling-wrapper row flex-row flex-nowrap py-2">
-                <HorizontalScroll categoryList={prepTime} />
+                <HorizontalScrollImg categoryList={prepTimeImg} />
             </div>
         </Fragment>
     );
@@ -129,6 +166,23 @@ const HorizontalScroll = (props) => {
                     data-holder-rendered="true"
                 />
                 <p className="pt-2">{category}</p>
+            </Link>
+        </div>
+    ));
+};
+
+const HorizontalScrollImg = (props) => {
+    const { categoryList } = props;
+
+    return categoryList.map((category, index) => (
+        <div className="col-2 my-col" key={index}>
+            <Link to="/recipes">
+                <img
+                    alt="100x100"
+                    src={category.img}
+                    data-holder-rendered="true"
+                />
+                <p className="pt-2">{category.name}</p>
             </Link>
         </div>
     ));
