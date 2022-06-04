931011cd49edefe27cb9ae4348dfec00fa61680e
esong165 <eysong@ucsd.edu>
2022-06-03 17:51:18 -0700

    added unit tests for getRecipe, getRecipeIds

--- test/firebase.test.js
+++ test/firebase.test.js
@@ -11,15 +11,49 @@ import { getFirestore, collection, doc, getDoc, getDocs, setDoc, updateDoc} from
 import assert from "assert";
 
 describe("firebase unit tests", () => {
-    it("getRecipe", async () => {
+    it("getRecipe with recipe in database", async () => {
         const recipe = await getRecipe("1095806");
         assert.equal(recipe.title, "Spanish style salmon fillets");
         // expect(recipe.title).equal("Spanish style salmon fillets");
-        const recipe2 = await getRecipe("644504");
-        assert.equal(recipe2.title, "German White Chocolate Cake");
-    });
-    it("getRecipeIds", () => {
-        const ids = getRecipeIds("");
     });
 
+    it("getRecipe with recipe not in database", async () => {
+        const recipe = await getRecipe("1");
+        assert.strictEqual(recipe, undefined);
+    });
+
+    it("getRecipeIds with cuisine in database", async () => {
+        const ids = await getRecipeIds("cuisines", "test");
+        assert.deepEqual(ids, [1, 2, 3]);
+    });
+
+    it("getRecipeIds with cuisine not in database", async () => {
+        const ids = await getRecipeIds("cuisines", "doesnotexist");
+        assert.equal(ids.length, 0);
+    });
+
+    it("getRecipeIds with ingredients in database", async () => {
+        const ids = await getRecipeIds("ingredients", "test");
+        assert.deepEqual(ids, [4, 5]);
+    });
+
+    it("getRecipeIds with ingredients not in database", async () => {
+        const ids = await getRecipeIds("ingredients", "doesnotexist");
+        assert.equal(ids.length, 0);
+    });
+
+    it("getRecipeIds with time in database", async () => {
+        const ids = await getRecipeIds("time", "test");
+        assert.deepEqual(ids, [6, 7, 8, 9]);
+    });
+
+    it("getRecipeIds with time not in database", async () => {
+        const ids = await getRecipeIds("time", "doesnotexist");
+        assert.equal(ids.length, 0);
+    });
+
+    it("getRecipeIds with category not in database", async () => {
+        const ids = await getRecipeIds("doesnotexist", "doesnotexist");
+        assert.equal(ids.length, 0);
+    });
 });
