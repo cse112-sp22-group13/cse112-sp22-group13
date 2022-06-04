2243f19a143e58756a6c0433e77efcb16478fd7d
esong165 <eysong@ucsd.edu>
2022-06-03 18:05:10 -0700

    Reorganized test file structure and reenabled e2e tests

--- test/e2e/frontPage.test.js
+++ test/e2e/frontPage.test.js
@@ -1,21 +1,21 @@
-// describe("Main.jsx", () => {
+describe("Main.jsx", () => {
 
-//     beforeAll(async () => {
-//         await page.goto("http://localhost:3000/");
-//     });
+    beforeAll(async () => {
+        await page.goto("http://localhost:3000/");
+    });
 
-//     it("All sections exist", async () => {
-//         await page.waitForSelector("h4");
+    it("All sections exist", async () => {
+        await page.waitForSelector("h4");
 
-//         const cuisine = await page.$$eval("h4", (els) => els[0].textContent);
-//         expect(cuisine).toBe("CUISINE");
+        const cuisine = await page.$$eval("h4", (els) => els[0].textContent);
+        expect(cuisine).toBe("CUISINE");
  
-//         const ingreds = await page.$$eval("h4", (els) => els[1].textContent);
-//         expect(ingreds).toBe("INGREDIENTS");
+        const ingreds = await page.$$eval("h4", (els) => els[1].textContent);
+        expect(ingreds).toBe("INGREDIENTS");
  
-//         const prep = await page.$$eval("h4", (els) => els[2].textContent);
-//         expect(prep).toBe("PREP TIME");
+        const prep = await page.$$eval("h4", (els) => els[2].textContent);
+        expect(prep).toBe("PREP TIME");
     
-//     });
+    });
     
-// });
+});
